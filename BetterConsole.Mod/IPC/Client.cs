42958c51ce20e8a5e4872dbbdc15135c15339da5
TylerGoeringer <tyler.goeringer@gmail.com>
2022-08-16 13:29:01 -0700

    Create Mod project / delete unused projects

---
+++ BetterConsole.Mod/IPC/Client.cs
@@ -0,0 +1,58 @@
+﻿using BetterConsole.Common;
+using H.Formatters;
+using H.Pipes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsole.Mod.IPC
+{
+  public class Client : IDisposable
+  {
+    private static Client _instance;
+    public static Client Instance => _instance ??= new Client();
+
+    private PipeClient<PipeMessage> BackingClient;
+
+    public async Task InitializeAsync()
+    {
+      if (BackingClient is not null && BackingClient.IsConnected) { return; }
+
+      Main.Logger.Log("Initiating mod connection.");
+
+      BackingClient = new(PipeMessage.PipeName, formatter: new NewtonsoftJsonFormatter());
+      BackingClient.Disconnected += (o, args) => Main.Logger.Log("Disconnected from server.");
+      BackingClient.Connected += (o, args) => Main.Logger.Log("Connected to server.");
+      BackingClient.ExceptionOccurred += (o, args) => OnException(args.Exception);
+
+      await BackingClient.ConnectAsync();
+
+      Main.Logger.Log("Mod connection established.");
+      SendText("Mod connection established.");
+    }
+
+    public async void SendText(string text)
+    {
+      await BackingClient.WriteAsync(new()
+      {
+        Action = ActionType.SendText,
+        Text = text
+      });
+    }
+
+    private void OnException(Exception e)
+    {
+      Main.Logger.LogException("PipeClient triggered an exception.", e);
+    }
+
+    public void Dispose()
+    {
+      if (BackingClient is not null)
+      {
+        BackingClient.DisposeAsync().GetAwaiter().GetResult();
+      }
+    }
+  }
+}
---
+++ BetterConsole.Mod/Main.cs
@@ -0,0 +1,43 @@
+﻿using BetterConsole.Mod.IPC;
+using HarmonyLib;
+using System;
+using static UnityModManagerNet.UnityModManager;
+using static UnityModManagerNet.UnityModManager.ModEntry;
+
+namespace BetterConsole.Mod
+{
+  public static class Main
+  {
+    public static ModLogger Logger;
+
+    private static Harmony Harmony;
+
+    public static bool Load(ModEntry modEntry)
+    {
+      try
+      {
+        Logger = modEntry.Logger;
+        modEntry.OnUnload = OnUnload;
+
+        Harmony = new Harmony(modEntry.Info.Id);
+        Harmony.PatchAll();
+
+        Client.Instance.InitializeAsync().GetAwaiter().GetResult();
+
+        Logger.Log("Finished patching.");
+      }
+      catch (Exception e)
+      {
+        Logger.LogException("Failed to patch", e);
+      }
+      return true;
+    }
+
+    private static bool OnUnload(ModEntry modEntry)
+    {
+      Client.Instance.Dispose();
+      Harmony.UnpatchAll();
+      return true;
+    }
+  }
+}
